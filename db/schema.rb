84b23055aaf67c68124324d8684df4646279a464
Shia <rise.shia@gmail.com>
2016-05-05 15:53:33 +0900

    add email to restore

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160505062446) do
   add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
 
   create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "username",                            null: false
     t.string   "reset_password_token"
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
     sequence(:username) { |n| "username#{n}" }
+    sequence(:email) { |n| "email@email.com" }
     sequence(:password) { |n| "password#{n}" }
   end
 end
